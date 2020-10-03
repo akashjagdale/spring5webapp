52a5a6246e50563e56d76b6a13ac18048d9c6f17
akashjagdale <adjagdale25@gmail.com>
2020-10-03 19:12:14 +0530

    Removed unnecessary AuthorRepository1.java

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository1.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.spring5webapp.repositories;
-
-import guru.springframework.spring5webapp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository1 extends CrudRepository<Author, Long> {
-}
